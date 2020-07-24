159f966aead1fe75b3a5b9390a8cff8bed064b28
Guilherme Brunner <guilherme.brunner@dtidigital.com.br>
2020-07-24 09:13:36 -0300

    Updates

---
+++ precache-manifest.5a658d0c4ea24393a3a5b384cbbed586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ab13ae08b602189b4de3030903bad4",
+    "url": "/smarttbot-challenge/index.html"
+  },
+  {
+    "revision": "9553f5392c437737e391",
+    "url": "/smarttbot-challenge/static/css/main.6ec9134e.chunk.css"
+  },
+  {
+    "revision": "fbc6e92348504df1c0e7",
+    "url": "/smarttbot-challenge/static/js/2.48f7e2d9.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/smarttbot-challenge/static/js/2.48f7e2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9553f5392c437737e391",
+    "url": "/smarttbot-challenge/static/js/main.b7807d53.chunk.js"
+  },
+  {
+    "revision": "4acc2fcc6dcd91e1a911",
+    "url": "/smarttbot-challenge/static/js/runtime-main.8e6b841f.js"
+  }
+]);
